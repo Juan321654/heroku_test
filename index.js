cf401063343ae08449dd2b447cbcb3d8e0fb0e32
juan camacho <juanc@tiv.solutions>
2022-02-17 19:46:24 -0500

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/contact", (req, res) => res.send("contact"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
